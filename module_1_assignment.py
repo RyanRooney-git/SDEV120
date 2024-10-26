d6d2ffe21662f825bea6cbfd5ce920d29e8ebe0b
Ryan Rooney <ryan.rooney42@yahoo.com>
2024-10-26 18:10:17 -0400

    Create module_1_assignment.py

---
+++ module_1_assignment.py
@@ -0,0 +1,28 @@
+
+// This pseudocode is intended to compute the number
+// of miles per gallon you get with your automobile
+
+start
+    input milesTraveled
+    input gallonsOfGas
+    milesPerGallon = milesTraveled / gallonsOfGas
+    output milesPerGallon
+
+stop
+
+
+// This pseudocode is intended to describe
+// computing the daily cost of your rent
+// in a 30-day month
+
+start
+    
+
+stop
+
+// This program accepts a user's monthly pay
+// and rent, utilities, and grocery bills
+// and displays the amount available for discretionary spending
+// (which might be negative)
+// Your program should output the pay and the total bills
+// as well as the remaining discretionary amount
