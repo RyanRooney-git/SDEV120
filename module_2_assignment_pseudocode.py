399c5430f0ded610d8ec49cca71557ce1e81df36
Ryan Rooney <ryan.rooney42@yahoo.com>
2024-11-03 00:26:44 -0400

    Update module_2_assignment_pseudocode.py

--- module_2_assignment_pseudocode.py
+++ module_2_assignment_pseudocode.py
@@ -1,5 +1,5 @@
 // This pseudocode is intented to
-// ask the user about their refrigerator model, name
+// ask the user about their refrigerator model name,
 // height, width, and depth in inches and then print
 
 
@@ -8,8 +8,8 @@ start
     input modelIntHeight = "Please enter the refrigerator model interior height in inches:"
     input modelWidth = "Please enter the refrigerator model width in inches:"
     input modelDepth = "Please enter the refrigerator model depth in inches:"
-    calculate refrigeratorCapacity
+    // calculate refrigeratorCapacity
     refrigeratorCapacity = (modelIntHeight * modelWidth * modelDepth) / 1728
-    output modelName + refrigeratorCapacity
+    output print "The refrigerator model name is:" + modelName "and the capacity is" + refrigeratorCapacity
     
 end
