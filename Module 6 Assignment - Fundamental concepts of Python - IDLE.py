a01f91018d53ded06bac9ac456ae7d41d674cad2
Ryan Rooney <145315125+RyanRooney-git@users.noreply.github.com>
2024-11-29 14:41:24 -0500

    Add files via upload

---
+++ Module 6 Assignment - Fundamental concepts of Python - IDLE.py
@@ -0,0 +1,10 @@
+
+# This program is intended to print the string "It is raining cats and dogs."
+
+# assigning the variables
+c = "cats"
+d = "dogs."
+# assigns the variable "s" with the values of "c" added after the string "It is raining " and "d"
+s = "It is raining " + c + " and " + d
+# prints the line "It is raining cats and dogs."
+print(s)
